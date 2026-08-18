e0391ba40524079746c80a93f806a7cde8dacf9c
Abir Al Murad <145911033+Abir-Al-Murad@users.noreply.github.com>
2026-08-18 14:11:00 +0600

    Delete lib/seller directory

--- lib/seller/dashborad/providers/seller_dashboard_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ebazarx/features/dashboard/domain/usecases/get_seller_dashboard_usecase.dart';
-import 'package:ebazarx/features/dashboard/presentation/providers/dashboard_providers.dart';
-import 'package:ebazarx/seller/dashborad/notifiers/seller_dashboard_notifier.dart';
-import 'package:ebazarx/seller/dashborad/states/seller_dashboard_state.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final getSellerDashboardUseCaseProvider = Provider((ref)=>GetSellerDashboardUseCase(ref.read(dashboardRepositoryProvider)));
-final sellerDashboardNotifierProvider = StateNotifierProvider<SellerDashboardNotifier, SellerDashboardState>((ref) => SellerDashboardNotifier(ref.read(getSellerDashboardUseCaseProvider)));
